e81d5f941d3229ed65cc540797b5486a8229852f
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-08-07 15:35:10 +0200

    chore: GET&POST

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,36 @@
-<?php
-declare(strict_types=1);
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'It works !';
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>GET - POST</title>
+</head>
+
+<body>
+
+	<form method="post" action="index.php">
+	  <input type="text" name="name">
+	  <input type="text" name="lang">
+	  <input type="submit" value="submit">
+	</form>
+
+    <?php
+	$name = $_POST['name'];
+	$lang = $_POST['lang'];
+	echo '<p>' . $name . '</p>';
+	echo '<p>' . $lang . '</p>';
+?>
+<?php
+ function greeting() {
+    $name = $_POST['name'];
+    echo "Hello, " . $name;
+ }
+ greeting();
+
+?>
+
+</body>
+
+</html>
